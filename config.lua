305b3c1cf5d326d99d73375f9927b0eef952e20d
I Made Gusti Agung <75777108+Gustiagung19@users.noreply.github.com>
2022-10-04 02:24:07 +0800

    Update config.lua add phone compatible v1.1.0

--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 Config = {}
 
-Config.gcphoneEnabled = true
+Config.gcphoneEnabled = false -- Set to true if you are using gcphone
+Config.gksphoneEnabled = true -- Set to true if you are using gksphone
 Config.UseAnimation = true
 Config.TimeToChange = 10
 Config.CheckForUpdates = true -- Set it to true if you want to get the latest version update info
