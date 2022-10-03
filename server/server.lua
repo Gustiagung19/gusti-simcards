5addc88a5498093e4dc6f439704a18d82c4c83fb
I Made Gusti Agung <75777108+Gustiagung19@users.noreply.github.com>
2022-10-04 02:23:20 +0800

    Update server.lua add phone compatible v1.1.0

--- server/server.lua
+++ server/server.lua
@@ -4,28 +4,56 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('gusti_simcards:useSimCard')
 AddEventHandler('gusti_simcards:useSimCard', function(number)
+if Config.gcphoneEnabled then
     local _source = source
     local rawNumber = number
     local xPlayer = ESX.GetPlayerFromId(_source)
     local numFirstThree = string.sub(rawNumber, 1, 3)
     local numLastFour = string.sub(rawNumber, 4, 7)
-    local numFinal = numFirstThree .. '-' .. numLastFour
+    local numFinal = numFirstThree .. numLastFour
     local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE phone_number = @phone_number", {['@phone_number'] = numFinal})
     if result[1] ~= nil then
         TriggerClientEvent('esx:showNotification', _source, '~r~That number is already in use')
     else       
         TriggerClientEvent('gusti_simcards:startNumChange', _source, numFinal)
     end     
+end
+end)
+
+RegisterServerEvent('gusti_simcards:useSimCard')
+AddEventHandler('gusti_simcards:useSimCard', function(number)
+    if Config.gksphoneEnabled then
+    local _source = source
+    local rawNumber = number
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local numFirstThree = string.sub(rawNumber, 1, 3)
+    local numLastFour = string.sub(rawNumber, 4, 7)
+    local numFinal = numFirstThree .. numLastFour
+    local result = MySQL.Sync.fetchAll("SELECT * FROM gksphone_settings WHERE phone_number = @phone_number", {['@phone_number'] = numFinal})
+    if result[1] ~= nil then
+        TriggerClientEvent('esx:showNotification', _source, '~r~That number is already in use')
+    else       
+        TriggerClientEvent('gusti_simcards:startNumChange', _source, numFinal)
+    end    
+end 
 end)
 
 RegisterServerEvent('gusti_simcards:changeNumber')
 AddEventHandler('gusti_simcards:changeNumber', function(newNum)
     local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)               
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    if Config.gcphoneEnabled then
     MySQL.Async.execute('UPDATE users SET phone_number = @phone_number WHERE identifier = @identifier', {
         ['@identifier'] = xPlayer.identifier,
         ['@phone_number'] = newNum
     })
+end
+    if Config.gksphoneEnabled then
+    MySQL.Async.execute('UPDATE gksphone_settings SET phone_number = @phone_number WHERE identifier = @identifier', {
+        ['@identifier'] = xPlayer.identifier,
+        ['@phone_number'] = newNum
+    })
+end
     xPlayer.removeInventoryItem('sim_card', 1)
     TriggerClientEvent('gusti_simcards:success', _source, newNum)
 end)
@@ -39,13 +67,13 @@ end)
 --[[ Version Checker ]]--
 local version = "1.0.0"
 
-if Config.CheckForUpdates then
-    AddEventHandler("onResourceStart", function(resource)
+AddEventHandler("onResourceStart", function(resource)
+    if Config.CheckForUpdates then
         if resource == GetCurrentResourceName() then
             CheckFrameworkVersion()
         end
-    end)
-end
+    end
+end)
 
 function CheckFrameworkVersion()
     PerformHttpRequest("https://raw.githubusercontent.com/Gustiagung19/gusti-simcards/master/version.txt", function(err, text, headers)
